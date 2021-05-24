620f368285d82e6b1dbc09f7953263e170f5565f
Nikolay <nikolayb33@gmail.com>
2021-05-24 18:52:48 +0300

    my work

---
+++ 2_1_1_task.py
@@ -0,0 +1,13 @@
+# Примечание: Список для проверки введите вручную. Или возьмите этот: [1, 2, 3, 4]
+from random import choice
+
+
+def random_elements(current_list):
+    if current_list == list:
+        return print(choice(current_list))
+    else:
+        None
+
+
+if __name__ == "__main__":
+    random_elements([1, 2, 3, 4])
